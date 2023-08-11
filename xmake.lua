226ad78724c9e36a2719f3313595dbd89722ee0b
hopenobug <87173134+hopenobug@users.noreply.github.com>
2023-08-11 20:25:41 +0800

    fix compile error

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,13 @@
 add_rules("mode.debug", "mode.release")
-set_languages("c99", "c++14")
+set_languages("c99", "c++11")
 
 target("demo")    
     set_kind("binary")
     add_files("*.cpp")
     add_linkdirs("frida")
-    add_links("frida-gum")    
+    add_links("frida-gum")
+    if is_plat("linux") then
+        add_links("dl", "pthread", "resolv")
+    end
     add_cxxflags("-O0", "-g", "-fno-lto", "-fno-inline-functions", "-fno-inline")
 
