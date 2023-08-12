22456d883828c04dfcf810857439209521719cb3
hopenobug <87173134+hopenobug@users.noreply.github.com>
2023-08-12 10:46:00 +0800

    add link

--- xmake.lua
+++ xmake.lua
@@ -8,6 +8,11 @@ target("demo")
     add_links("frida-gum")
     if is_plat("linux") then
         add_links("dl", "pthread", "resolv")
+    elseif is_plat("windows") then 
+        add_defines("NOMINMAX")
+        add_cxxflags("/utf-8")
+        add_cxxflags("/Zc:__cplusplus")
+        add_links("Advapi32", "Ole32", "Shell32", "User32")
     end
     add_cxxflags("-O0", "-g", "-fno-lto", "-fno-inline-functions", "-fno-inline")
 
